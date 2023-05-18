863b4476395d2242e6fbe64b77d48a836c1dff73
freemjstudio <freemjstudio>
2023-05-18 05:18:57 +0000

    aws connect

--- From AWS S3 to Databricks tables.py
+++ From AWS S3 to Databricks tables.py
@@ -8,42 +8,57 @@
 
 # df.write.option("path","s3://freemjstudio-bucket/export-s3/export_info_export-s3.json").saveAsTable("my_table")
 
-# spark.sql("""
-#     CREATE TABLE my_table
-#     AS SELECT * 
-#     FROM json.`s3://freemjstudio-bucket/export-s3/export_info_export-s3.json`
-# """)
+df = spark.sql("""
+    CREATE TABLE my_table
+    AS SELECT * 
+    FROM parquet.`s3://freemjstudio-bucket/testdb/member_table/LOAD00000001.parquet`
+""")
 
 
 # https://medium.com/grabngoinfo/databricks-mount-to-aws-s3-and-import-data-4100621a63fd
 
 # COMMAND ----------
 
+# MAGIC %sql
+# MAGIC desc detail test_access_keys
+
+# COMMAND ----------
+
+dbutils.fs.ls("dbfs:/user/hive/warehouse/test_access_keys")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *  # pyspark functions 
+
+# URL processing 
+import urllib 
+
+file_type = "parquet" 
+
+# Whether the file has a header
+first_row_is_header = "true"
+
+# Delimiter used in the file 
+delimiter = ","
+
+# Read CSV file to spark dataframe 
+aws_keys_df = spark.read.format(file_type).option("header", first_row_is_header).option("sep", delimiter).load("dbfs:/user/hive/warehouse/test_access_keys")
+
+
+# COMMAND ----------
+
+# Get the AWS access key and secret key from the spark dataframe 
+ACCESS_KEY = aws_keys_df.where(col)
+
+# COMMAND ----------
+
 aws_bucket_name = "freemjstudio-bucket"
 
-df = spark.read.format("json").load(f"s3://{aws_bucket_name}/export-s3/export_info_export-s3.json")
+df = spark.read.format("parquet").load(f"s3://{aws_bucket_name}/testdb/member_table/LOAD00000001.parquet")
 display(df)
 dbutils.fs.ls(f"s3://{aws_bucket_name}/")
 
 # SELECT * FROM json.`$path.json`
-
-# COMMAND ----------
-
-# Mount S3 Bucket 
-
-from pyspark.sql.functions import *
-import urllib
-
-# file type 
-file_type = "csv"
-
-first_row_is_header = "true"
-delimiter = ","
-
-# Read CSV file to spark dataframe 
-
-aws_keys_df = spark.read.format(file_type).option("header", first_row_is_header).option("sep", delimiter).load("")
-
 
 # COMMAND ----------
 
