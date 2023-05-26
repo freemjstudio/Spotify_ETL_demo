2970964b4fd0a4bdfd383a6ab6b029382fcd3bf6
freemjstudio <mjwoo0@naver.com>
2023-05-26 15:43:33 +0900

    response

--- Spotify_ETL/get_data.py
+++ Spotify_ETL/get_data.py
@@ -1,15 +1,31 @@
 # Get Data from Spotify 
 
-from test import * 
+from identification import * 
 
 import spotipy 
 from spotipy.oauth2 import SpotifyClientCredentials
+from pprint import pprint 
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
                                                            client_secret=CLIENT_SECRET))
 
-results = sp.search(q='Oasis', limit=20)
 
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx, track['name'])
+pid = "spotify:playlist:37i9dQZEVXbNxXF4SkHj9F"
+offset = 0 
+
+playlist_list = []
+
+while True:
+
+    response = sp.playlist_items(playlist_id=pid,
+                                 offset=offset,
+                                 additional_types=['track'])
     
+    
+    break 
+    # if len(response['track']['items']) == 0:
+    #     break 
+
+    # print(response['track']['items'])
+    # offset = offset + len(response['track']['items'])
+    # print(offset, "/", response["total"])
