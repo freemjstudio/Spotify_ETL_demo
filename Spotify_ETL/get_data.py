1168796475279b0aaff38bdddb791179f937645b
freemjstudio <mjwoo0@naver.com>
2023-05-30 13:55:07 +0900

    parsing

--- Spotify_ETL/get_data.py
+++ Spotify_ETL/get_data.py
@@ -5,9 +5,7 @@ from identification import *
 import spotipy 
 from spotipy.oauth2 import SpotifyClientCredentials
 from pprint import pprint 
-import pandas as pd 
-
-
+import json
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
                                                            client_secret=CLIENT_SECRET))
@@ -16,22 +14,29 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
 pid = "spotify:playlist:37i9dQZEVXbNxXF4SkHj9F"
 offset = 0 
 
+
 album_names = []
-while True:
-    response = sp.playlist_items(playlist_id=pid,
+artist_names = []
+response = sp.playlist_items(playlist_id=pid,
                                  offset=offset,
                                  additional_types=['track'])
-    print(type(response))
-   # pd_object = pd.read_json(response, type='series')
-    # for item in response['items']:
-    #     artist_names = []
-        
-    #     for artist in item['track']['album']['artists']:
-    #         artist_names.append(artist['name'])
-        
-    #     album_names.append(item['track']['album']['name'])
 
-    
-    break 
+for item in response['items']:
+    artist_list = []
+        
+    for artist in item['track']['album']['artists']:
+        artist_list.append(artist['name'])
+    artist_names.append(artist_list)
+    album_names.append(item['track']['album']['name'])
 
-# 50 개 다 수집되었는지 확인 
+
+
+result= dict()
+for i in range(50):
+    temp = dict()
+    temp['album'] = album_names[i]
+    temp['artist'] = artist_names[i][0]
+
+    result[i] = temp
+
+print(result)
