81133d0b46373af642964b046df8954ee8a2687e
freemjstudio <mjwoo0@naver.com>
2023-06-01 15:50:52 +0900

    add load aws s3 bucket data

---
+++ Databricks/Load_AWS_S3.py
@@ -0,0 +1,11 @@
+ACCESS_KEY = ""
+SECRET_KEY = ""
+ENCODED_SECRET_KEY = SECRET_KEY.replace("/", "%2F")
+AWS_BUCKET_NAME = "cake-demo"
+MOUNT_FOLDER = "s3_cake"
+
+MOUNT_DIR = "s3a://{0}:{1}@{2}".format(ACCESS_KEY, ENCODED_SECRET_KEY, AWS_BUCKET_NAME)
+dbutils.fs.mount(MOUNT_DIR, f"/mnt/minjiwoo/{MOUNT_FOLDER}")
+display(dbutils.fs.ls(f"/mnt/{MOUNT_FOLDER}"))
+
+df = spark.read.format("csv").load(f"dbfs:/mnt/minjiwoo/{MOUNT_FOLDER}/")
