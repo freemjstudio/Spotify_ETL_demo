2a03d980648024ae11ca879e935a2b86833ec6ce
freemjstudio <mjwoo0@naver.com>
2023-06-19 09:38:45 +0900

    0619 etl

--- Spotify_ETL/GetJSONData.py
+++ Spotify_ETL/GetJSONData.py
@@ -9,17 +9,19 @@ import json
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
                                                            client_secret=CLIENT_SECRET))
 
-playlist_id = "spotify:playlist:37i9dQZEVXbNxXF4SkHj9F" # Korea Spotify Top 50
+kr_playlist_id = "spotify:playlist:37i9dQZEVXbNxXF4SkHj9F" # Korea Spotify Top 50
+jp_playlist_id = "spotify:playlist:37i9dQZEVXbKXQ4mDTEBXq" # Japan Spotify Top 50
+
 today = datetime.today().strftime('%Y-%m-%d')
 data = {}
-data[f'{today}'] = []
-response = sp.playlist_tracks(playlist_id)
+data[today] = []
+response = sp.playlist_tracks(jp_playlist_id)
 tracks = response['items']
-data[f'{today}'].append(tracks)
+data[today].append(tracks)
 
 
 # .json 파일로 export 하기
-file_path = "./playlist.json"
+file_path = "./jp_playlist2.json"
 print(data)
 
 with open(file_path, "w") as outfile:
